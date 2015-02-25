5adf8894250bed2a555de5f8a2d0627748d2efee
jdjaymalin <jade.jaymalin@gmail.com>
2015-02-25 10:29:36 -0600

    Update and rename SPOJ to SPOJ/ONP.pl

---
+++ SPOJ/ONP.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @stack;
+my @operators = ('+', '-', '*', '/', '^');
+my @inputs;
+
+my $inputNo = <>;
+chomp $inputNo;
+
+for (my $i=0; $i<$inputNo; $i++){
+    my $exp = <>;
+    chomp $exp;
+
+    my $expression;
+    my @chars = split('',$exp);
+
+    foreach my $char (@chars){
+        if ($char eq '('){
+                next;
+        }
+        if ($char eq ')'){
+                my $op = pop(@stack);
+                $expression = $expression . $op;
+                next;
+        }
+        if ($char ~~ @operators){
+                push (@stack, $char);
+                next;
+        }
+        if ($char =~ /^[A-za-z]$/) {
+                $expression = $expression . $char;
+                next;
+        }
+    }
+    print "$expression\n";
+}
