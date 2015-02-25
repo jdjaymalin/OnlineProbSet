8d23aa0e9732fc33d54c0d5f6950117cbe30c6a1
jdjaymalin <jade.jaymalin@gmail.com>
2015-02-25 10:26:23 -0600

    Update and rename SPOJ to SPOJ/Prime1.pl

---
+++ SPOJ/Prime1.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub generate_prime{
+    my ($min,$max) = @_;
+    my %primes;
+
+    for (my $i=2; $i<=$max; $i++) {
+        my $p = $i;
+        if (!exists $primes{$p}){
+            $primes{$p} = 1;
+        }
+
+        while ($p*$p <= $max){
+            my $j = $p*$p;
+
+            while ($j <= $max){
+                $primes{$j} = 0;
+                $j = $j+$p;
+            }
+            $p++ unless (exists $primes{$p++})
+        }
+    }
+
+    # Ouput printing
+    for my $prime (sort {$a <=> $b} keys %primes){
+        if ($min <= $prime && $primes{$prime} eq 1) {
+             print "$prime\n";
+        }
+    }
+    print "\n";
+}
+
+my $inputNo = <>;
+chomp $inputNo;
+my %primes;
+
+for (my $i=0; $i<$inputNo; $i++) {
+    my $line = <>;
+    my ($first, $last) = split(' ', $line);
+    generate_prime($first,$last);
+}
