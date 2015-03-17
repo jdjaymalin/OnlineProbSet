6cfa14d5f55029f7c1c7743ec47048978f626282
jdjaymalin <jade.jaymalin@gmail.com>
2015-03-17 16:16:09 -0500

    Create fctrl.pl

---
+++ SPOJ/fctrl.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $inputNo = <>;
+chomp $inputNo;
+
+for (my $i=0; $i<$inputNo; $i++){
+    my $input = <>;
+    chomp $input;
+
+    my $divisor = 5;
+    my $count = 0;
+    my $quo = 1;
+
+    while ($quo >= 1) {
+        $quo = $input/$divisor;
+        $divisor *= 5;
+        $count = $count + int $quo;
+    }
+    print $count . "\n";
+}
